9d9e2689e28f6980367fc240c4aef78184942afd
Christoph Knoth <github@christoph-knoth.com>
2023-11-24 13:57:37 +0100

    imprint fix

--- components/layout.js
+++ components/layout.js
@@ -185,6 +185,20 @@ const Layout = ({ children, global, festival}) => {
       });
 
 
+      var closer = $("<div class='closer'>x</div>");
+
+      closer.click(function(){
+          $(this).closest(".closer-target").hide();
+      });
+
+      $("#imprint-content").addClass("closer-target").append(closer.clone(true));
+
+      $("#imprint-link").click(function(e){
+        e.preventDefault();
+        $("#imprint-content").show();
+      });  
+
+
       $(window).scroll(function() {
 
         //console.log($(window).scrollTop());
@@ -511,6 +525,31 @@ const Layout = ({ children, global, festival}) => {
           </div>
         </div>
       }
+
+
+
+      <div id="imprint-content" class="closer-target">
+        <div class="imprint-inner">
+          <p>
+              <a href="https://www.knoth-renner.com/" target="_blank">Knoth &amp; Renner</a> teamed up with <a href="https://www.instagram.com/aeni.kaiser/" target="_blank">Anja Kaiser</a> for the identity and concept of this website, working with Janine Zielman on its development.
+          </p>
+          
+          <p>
+          Based on a deep appreciation for technology and its visual echo, the Berlin based design studio Knoth &amp; Renner work on the intersection of digital culture and graphic design.
+          </p>
+          <p>
+          Anja Kaiser is a graphic designer and artist based in Leipzig. In her practice, she embraces the relational strategies of ‘dirty typography’ - disrupting, expanding, and joining forces between graphics and typography. 
+          </p>
+          <p>
+              Typeface: Coda by <a href="https://annacairns.com/" target="_blank">Anna Cairns</a>
+          </p>
+          <p>
+              Sonic Acts is responsible for all content published on this website.
+          </p>
+        </div>    
+      </div>
+
+
 
       <div id="imprint-menu">  
 
