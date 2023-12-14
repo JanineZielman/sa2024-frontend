2cbc4eba4992158f6d277c85a899e8150235ffd3
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2023-12-14 21:03:36 +0100

    clean up

---
+++ next-sitemap.config.js
@@ -0,0 +1,15 @@
+// const siteUrl = 'https://2024.sonicacts.com/';
+// module.exports = {
+//   siteUrl,
+//   exclude: ["/404"],
+//   generateRobotsTxt: true,
+//   robotsTxtOptions: {
+//     policies: [
+//       {
+//         userAgent: "*",
+//         disallow: ["/404"],
+//       },
+//       { userAgent: "*", allow: "/" },
+//     ],
+//   },
+// };
