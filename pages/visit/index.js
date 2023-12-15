c7f2c9637bf758e3d58c943fd585259dc03b2fb9
Christoph Knoth <github@christoph-knoth.com>
2023-12-15 14:50:23 +0100

    add a fourth color to the map icons

--- pages/visit/index.js
+++ pages/visit/index.js
@@ -25,7 +25,7 @@ const Visit = ({ global, visit, festival, programmeLoc }) => {
           zoom: 12.3 // Set default zoom level
       });
 
-      var keycolors = ["#58de60", "#ff820a", "#ffbbf1"]; //green, orange, pink
+      var keycolors = ["#58de60", "#ff820a", "#ffbbf1", "#FFFFFF"]; //green, orange, pink
 
       let lastPopup = null; // To keep track of the current open popup
 
