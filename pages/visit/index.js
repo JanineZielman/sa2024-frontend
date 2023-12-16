d93012123eaa857bf7caa8d2001990230e83cef4
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2023-12-16 12:52:16 +0100

    visit page fix

--- pages/visit/index.js
+++ pages/visit/index.js
@@ -2,14 +2,13 @@ import React, {useState, useEffect} from "react"
 import ReactMarkdown from "react-markdown";
 import Layout from "../../components/layout"
 import { fetchAPI } from "../../lib/api"
-import Collapsible from 'react-collapsible';
 
 
-const Visit = ({ global, visit, festival, programmeLoc }) => {
+const Visit = ({ global, festival, programmeLoc }) => {
 
   let programmeLocations = programmeLoc.attributes.location_item;
 
-  
+  console.log(programmeLocations)
 
   useEffect(() => {
     setTimeout(function(){
@@ -99,51 +98,17 @@ const Visit = ({ global, visit, festival, programmeLoc }) => {
           <div className="content">
             <div className="wrapper">
               <div className="locations">
-                {visit.map((item, i) => {
+                {programmeLocations.map((item, i) => {
+                  console.log(item)
                   return(
-                    <>
-                    {item.locations &&
-                      <>
-                        {item.locations.data.map((loc, j) => {
-                          let locInfo =  programmeLocations?.filter((item) => item.title == loc.attributes.title);
-                          return(
-                            <div className="location" data-gps={loc.attributes.gps}>
-                              <h2>{loc.attributes.title} {loc.attributes.subtitle && <span> – {loc.attributes.subtitle} </span>}</h2>
-                              <h3>{loc.attributes.location}</h3>
-                              <ReactMarkdown className="opening-times" children={locInfo[0]?.opening_times}/>
-                              <ReactMarkdown className="additional-info"
-                                children={loc.attributes.additional_info} 
-                              />
-                            </div>
-                          )
-                        })}
-                      </>
-                    }
-
-                    {item.embed &&
-                      <div className="map">
-                        <div dangerouslySetInnerHTML={{__html: item.embed}}/>
-                      </div>
-                    }
-                    {item.text &&
-                      <div className="text-block">
-                        <ReactMarkdown 
-                          children={item.text} 
-                        />
-                      </div>
-                    }
-                    {item.__component == "biennial.collapsible" &&
-                      <div className="collapsible visit">
-                        <Collapsible trigger={item.title} open={item.open == true && item.open}>
-                          <div className={'text-block ' + item.size} key={'textcol'+i}>
-                            <ReactMarkdown 
-                              children={item.text_block} 
-                            />
-                          </div>
-                        </Collapsible>
-                      </div>
-                    }
-                  </>
+                    <div className="location" data-gps={item.location.data.attributes.gps}>
+                      <h2>{item.location.data.attributes.title} {item.location.data.attributes.subtitle && <span> – {item.location.data.attributes.subtitle} </span>}</h2>
+                      <h3>{item.location.data.attributes.location}</h3>
+                      <ReactMarkdown className="opening-times" children={item.opening_times}/>
+                      <ReactMarkdown className="additional-info"
+                        children={item.location.data.attributes.additional_info} 
+                      />
+                    </div>
                   )
                 })}
               </div>
@@ -162,18 +127,16 @@ export async function getServerSideProps() {
 		slug: "biennial-2024"
 	}
   // Run API calls in parallel
-  const [festivalRes, visitRes, globalRes, programmeLoc] = await Promise.all([
+  const [festivalRes, globalRes, programmeLoc] = await Promise.all([
     fetchAPI(`/biennials?filters[slug][$eq]=${params.slug}&populate[prefooter][populate]=*`),
-    fetchAPI(`/biennials?filters[slug][$eq]=${params.slug}&populate[visit][populate]=*`),
     fetchAPI("/global?populate[prefooter][populate]=*&populate[socials][populate]=*&populate[image][populate]=*&populate[footer_links][populate]=*&populate[favicon][populate]=*", { populate: "*" }),
-    fetchAPI(`/programme-pages?&populate[0]=location_item`),
+    fetchAPI(`/programme-pages?filters[slug][$eq]=programme-2024&populate[location_item][populate]=*`),
   ])
 
 
   return {
     props: {
       festival: festivalRes.data[0],
-      visit: visitRes.data[0].attributes.visit,
       global: globalRes.data,
       programmeLoc: programmeLoc.data[0]
     }
