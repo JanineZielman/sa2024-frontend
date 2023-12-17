de6f75bff4b16a546046068b8d206bd7e8005ceb
Christoph Knoth <github@christoph-knoth.com>
2023-12-17 22:22:52 +0100

    funny elements

--- components/layout.js
+++ components/layout.js
@@ -212,12 +212,59 @@ const Layout = ({ children, festival}) => {
         $("#imprint-content").show();
       });  
 
+      var scrollTop = 0;
+      var randomNumber = Math.random();
+
+      console.log(randomNumber);
+
+      function setColoredBG() {
+        if (randomNumber > 0.7) {
+          console.log("case 1");
+
+          $(".colored-bg-01").css({
+            "transform":"translateY(" + Math.min(scrollTop * 0.5, 300) + "px scale(" + Math.min(scrollTop / 2000 + 0.5, 5) + ")",
+            "background-color" : "#58de60",
+            "box-shadow": "0px 0px 2rem 2rem #58de60"
+          })
+
+          $(".colored-bg-02").css({
+            "transform":"translateY(" + (-1 * scrollTop * 0.5) + "px) scale(" + Math.max(-1 *scrollTop / 2000 + 0.5, 0.5) + ")",
+          })
+        }
+
+        if (randomNumber <= 0.7 && randomNumber > 0.3) {
+          console.log("case 2");
+          $(".colored-bg-01").css({
+            "transform":"translateY(" + -1 * Math.min(scrollTop * 0.5, 300) + "px)"
+          })
+
+          $(".colored-bg-02").css({
+            "transform":"translateY(" + (scrollTop * 0.5) + "px) scale(" + Math.min(scrollTop / 2000 + 0.5, 5) + ")",
+          })
+        }
+
+        if (randomNumber <= 0.3) {
+          console.log("case 3");
+          $(".colored-bg-01").css({
+            "transform":"translateY(" + Math.min(scrollTop * 0.5, 300) + "px)"
+          })
+
+          $(".colored-bg-02").css({
+            "transform":"translateY(" + (-1 * scrollTop * 0.5) + "px))",
+          })
+        }
+      }
+
+      setColoredBG();
+      setTimeout(function(){
+        setColoredBG();
+      }, 10);
 
       $(window).scroll(function() {
 
-        //console.log($(window).scrollTop());
+        console.log($(window).scrollTop());
 
-        var scrollTop = $(window).scrollTop();
+        scrollTop = $(window).scrollTop();
 
         $("#title-1-sonic, #title-1-sonic-mobile").css({
             "transform":"translate(-50%, calc(-50% - " + scrollTop * 0.4 + "px))"
@@ -249,6 +296,8 @@ const Layout = ({ children, festival}) => {
             "transform":"translateY(" + -1 * Math.min(scrollTop, $(window).width()*0.4) + "px)"
         })
 
+        setColoredBG();
+
         var maxScroll = $(window).width() * 0.1;
         var maxScale = $(window).width() * 0.17; //how small it gets, the smaller the number the smaller
 
@@ -277,57 +326,60 @@ const Layout = ({ children, festival}) => {
     <div id="b-2024" class="hide-at-pageload visible">
             <div id="b-2024-inner">
                 <div id="svg-biennial">
-    <svg version="1.1" id="Ebene_1" x="0px" y="0px" viewBox="0 0 5055.6 523.9">
+                  <svg version="1.1" id="Ebene_1" x="0px" y="0px" viewBox="0 0 5055.6 523.9">
 
-    <g id="bottom-biennial">
-    	<path id="bottom-biennial-3" class="st321408h124" d="M1314.9,160.2V498 M1271.8,498c-1-13-2.1-25-2.1-46.2V333.2c0-50.1-30.3-70.3-80-70.3
-    		c-42.1,0-72.9,22.2-85.7,41.9 M1261.8,355.9c0,0-37.7-2.9-65.4-2.9c-52.3,0-101.1,23.1-101.1,74.7c0,48.7,25.4,74.2,81.6,74.2
-    		s88.4-28.9,88.4-28.9 M1064,232.1v-71.9 M1064,498V267.2 M655.1,266.7V498 M815.7,498V332.3c0-40.5-26.9-68.9-80.8-68.9
-    		c-15.8,0-34.7,5.5-50.7,14.1l-24.1,17.7 M421.2,381.9h198.3c0,0,0.5-10.1,0-15.4c-5.1-64.1-42.6-103.1-101.1-103.1
-    		c-63.1,0-106.2,47.2-106.2,119.5c0,71.8,42.6,119.9,106.2,119.9 M518.4,502.8c50.3,0,88.8-30.8,99-80 M378.4,232.1v-71.9
-    		 M378.4,498V267.2 M73.6,306.4h188.6c29.5,0,60.8-25.7,60.8-69c0-44.6-34.8-76.6-96.4-76.6h-153V498H206
-    		c75.9,0,129.3-32.8,129.3-105c0-48-29.4-86.6-73.1-86.6"></path>
-    	<line id="bottom-biennial-2" class="st321408h124" x1="857.9" y1="266.7" x2="857.9" y2="498"></line>
-    	<path id="bottom-biennial-1" class="st321408h124" d="M1018.5,498V332.3c0-40.5-26.9-68.9-80.8-68.9c-15.8,0-34.7,5.5-50.7,14.1l-24.1,17.7"></path>
-    </g>
-    </svg>
+                  <g id="bottom-biennial">
+                    <path id="bottom-biennial-3" class="st321408h124" d="M1314.9,160.2V498 M1271.8,498c-1-13-2.1-25-2.1-46.2V333.2c0-50.1-30.3-70.3-80-70.3
+                      c-42.1,0-72.9,22.2-85.7,41.9 M1261.8,355.9c0,0-37.7-2.9-65.4-2.9c-52.3,0-101.1,23.1-101.1,74.7c0,48.7,25.4,74.2,81.6,74.2
+                      s88.4-28.9,88.4-28.9 M1064,232.1v-71.9 M1064,498V267.2 M655.1,266.7V498 M815.7,498V332.3c0-40.5-26.9-68.9-80.8-68.9
+                      c-15.8,0-34.7,5.5-50.7,14.1l-24.1,17.7 M421.2,381.9h198.3c0,0,0.5-10.1,0-15.4c-5.1-64.1-42.6-103.1-101.1-103.1
+                      c-63.1,0-106.2,47.2-106.2,119.5c0,71.8,42.6,119.9,106.2,119.9 M518.4,502.8c50.3,0,88.8-30.8,99-80 M378.4,232.1v-71.9
+                      M378.4,498V267.2 M73.6,306.4h188.6c29.5,0,60.8-25.7,60.8-69c0-44.6-34.8-76.6-96.4-76.6h-153V498H206
+                      c75.9,0,129.3-32.8,129.3-105c0-48-29.4-86.6-73.1-86.6"></path>
+                    <line id="bottom-biennial-2" class="st321408h124" x1="857.9" y1="266.7" x2="857.9" y2="498"></line>
+                    <path id="bottom-biennial-1" class="st321408h124" d="M1018.5,498V332.3c0-40.5-26.9-68.9-80.8-68.9c-15.8,0-34.7,5.5-50.7,14.1l-24.1,17.7"></path>
+                  </g>
+                  </svg>
 
-</div>
-
-
-<div id="svg-2024">
-
-    <svg version="1.1" id="Ebene_1" x="0px" y="0px" viewBox="0 0 5055.6 523.9">
-    <g id="bottom-2024">
-    	<path id="bottom-2024-2" class="st012308912h3" d="M4970.2,495.5V168h-23.3l-159.6,239.5l11,17.8h233.4 M4421.9,502.8
-    		c76.4,0,131.9-59.7,131.9-168.6c0-107.9-50.8-168.6-131.9-168.6c-81.6,0-132.9,60.7-132.9,168.6
-    		C4289,443,4344.9,502.8,4421.9,502.8 M4018.5,266.3c10.3-58.3,60.3-100.7,123.6-100.7c88.8,0,114.4,53.9,114.4,96.8
-    		c0,53.5-35.9,61.4-131.3,99.2s-102.4,67.1-119.3,117.7L4020,498h236"></path>
-    	<path id="bottom-2024-1" class="st012308912h3" d="M4586.5,266.3c10.3-58.3,60.3-100.7,123.6-100.7c88.8,0,114.4,43.9,114.4,86.8
-    		c0,53.5-35.9,71.4-131.3,109.2s-102.4,67.1-119.3,117.7L4588,498h236"></path>
-    </g>
-    </svg>
+              </div>
 
 
-</div>
+              <div id="svg-2024">
 
-<div id="svg-bottom-table">
+                  <svg version="1.1" id="Ebene_1" x="0px" y="0px" viewBox="0 0 5055.6 523.9">
+                  <g id="bottom-2024">
+                    <path id="bottom-2024-2" class="st012308912h3" d="M4970.2,495.5V168h-23.3l-159.6,239.5l11,17.8h233.4 M4421.9,502.8
+                      c76.4,0,131.9-59.7,131.9-168.6c0-107.9-50.8-168.6-131.9-168.6c-81.6,0-132.9,60.7-132.9,168.6
+                      C4289,443,4344.9,502.8,4421.9,502.8 M4018.5,266.3c10.3-58.3,60.3-100.7,123.6-100.7c88.8,0,114.4,53.9,114.4,96.8
+                      c0,53.5-35.9,61.4-131.3,99.2s-102.4,67.1-119.3,117.7L4020,498h236"></path>
+                    <path id="bottom-2024-1" class="st012308912h3" d="M4586.5,266.3c10.3-58.3,60.3-100.7,123.6-100.7c88.8,0,114.4,43.9,114.4,86.8
+                      c0,53.5-35.9,71.4-131.3,109.2s-102.4,67.1-119.3,117.7L4588,498h236"></path>
+                  </g>
+                  </svg>
+                                
+              </div>
 
-    <svg version="1.1" id="Ebene_1" x="0px" y="0px" viewBox="0 0 5055.6 523.9">
+              <div id="svg-bottom-table">
 
-    <g id="bottom-biennial-table">
-    	<line id="bottom-biennial-table-6" class="st1as9213h" x1="21.2" y1="56.5" x2="5031.7" y2="56.5"></line>
-    	<line id="bottom-biennial-table-5" class="st21203912jh3" x1="21.2" y1="524.2" x2="21.2" y2="56.5"></line>
-    	<line id="bottom-biennial-table-4" class="st1as9213h" x1="837.8" y1="524.2" x2="837.8" y2="56.5"></line>
-    	<line id="bottom-biennial-table-3" class="st1as9213h" x1="4042.7" y1="524.2" x2="4042.7" y2="56.5"></line>
-    	<line id="bottom-biennial-table-2" class="st1as9213h" x1="5031.7" y1="524.2" x2="5031.7" y2="56.5"></line>
-    	<line id="bottom-biennial-table-1" class="st1as9213h" x1="21.2" y1="377.7" x2="4042.7" y2="377.7"></line>
-    </g>
-    </svg>
+                  <svg version="1.1" id="Ebene_1" x="0px" y="0px" viewBox="0 0 5055.6 523.9">
+
+                  <g id="bottom-biennial-table">
+                    <line id="bottom-biennial-table-6" class="st1as9213h" x1="21.2" y1="56.5" x2="5031.7" y2="56.5"></line>
+                    <line id="bottom-biennial-table-5" class="st21203912jh3" x1="21.2" y1="524.2" x2="21.2" y2="56.5"></line>
+                    <line id="bottom-biennial-table-4" class="st1as9213h" x1="837.8" y1="524.2" x2="837.8" y2="56.5"></line>
+                    <line id="bottom-biennial-table-3" class="st1as9213h" x1="4042.7" y1="524.2" x2="4042.7" y2="56.5"></line>
+                    <line id="bottom-biennial-table-2" class="st1as9213h" x1="5031.7" y1="524.2" x2="5031.7" y2="56.5"></line>
+                    <line id="bottom-biennial-table-1" class="st1as9213h" x1="21.2" y1="377.7" x2="4042.7" y2="377.7"></line>
+                  </g>
+                  </svg>
 
 
-</div>            </div>
-        </div>
+              </div>
+          </div>
+    </div>
+
+    <div className="colored-bg-01"></div>
+    <div className="colored-bg-02"></div>
 
     <div id="social-homelink">
 
