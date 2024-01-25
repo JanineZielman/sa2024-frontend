6a1fa8d276581ef1ce150b979c8711f4080353fe
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2024-01-25 16:11:41 +0100

    sub page

--- pages/programme/[programme]/[sub].js
+++ pages/programme/[programme]/[sub].js
@@ -1,16 +1,38 @@
 import { fetchAPI } from "../../../lib/api"
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import Layout from "../../../components/layout"
 import BiennialArticle from "../../../components/biennial-article"
 import LazyLoad from 'react-lazyload';
 import Image from "../../../components/image"
 import Moment from "moment";
 
-const SubProgrammeItem = ({page, global, relations, params, festival, sub, programmeLoc}) => {
+const SubProgrammeItem = ({page, global, relations, params, sub, festival, programmeLoc}) => {
 
-  let programmeLocations = programmeLoc.attributes.location_item
+  let programmeLocations = programmeLoc.attributes.location_item;
+
+  const [subItems, setSubItems] = useState();
 
   useEffect(() => {
+
+    if (page.attributes.hide_when_where != true){
+      setSubItems(sub.sort((a, b) => {
+        if (a.attributes.title.toLowerCase() < b.attributes.title.toLowerCase()) {
+          return -1;
+        }
+        if (a.attributes.title.toLowerCase() > b.attributes.title.toLowerCase()) {
+          return 1;
+        }
+        return 0;
+      }))
+    } else {
+      setSubItems(sub.sort(function(a,b){
+        let date1 = new Date(`${a.attributes.WhenWhere[0]?.date.split('/').reverse().join('-')}T${a.attributes.WhenWhere[0]?.start_time}Z`);
+        let date2 = new Date(`${b.attributes.WhenWhere[0]?.date.split('/').reverse().join('-')}T${b.attributes.WhenWhere[0]?.start_time}Z`);
+        return date1 - date2
+
+      }));
+    }
+
     relations.attributes.community_items.data.sort((a, b) => {
       if (a.attributes.slug.toLowerCase() < b.attributes.slug.toLowerCase()) {
         return -1;
@@ -20,12 +42,87 @@ const SubProgrammeItem = ({page, global, relations, params, festival, sub, progr
       }
       return 0;
     })
-  }, [])
+
+  }, [subItems])
 
   return (  
-    <section className="festival-wrapper programme-sub">
+    <section className={`festival-wrapper ${params.programme}`}>
       <Layout global={global} festival={festival}>
-        <BiennialArticle page={page} relations={relations} programmeLocations={programmeLocations}/>
+        <BiennialArticle page={page} relations={relations} programmeLocations={programmeLocations} />
+        {subItems?.length > 0 && 
+          <> 
+            <div className="discover sub">
+              <div className="subtitle">
+                {relations.attributes.sub_programmes_title &&
+                  <h1>{relations.attributes.sub_programmes_title}</h1>
+                }
+              </div>
+              <div className="discover-container programme-container sub-programme-container">
+                <div className="day-programme">
+                  <div className="discover-container programme-container sub-programme-container">
+                    {subItems.map((item, i) => {
+                      let dates = item.attributes.WhenWhere?.sort((a,b)=>new Date(a.date).getTime()-new Date(b.date).getTime());
+                      let start_date = new Date(dates?.[0]?.date.split('/').reverse().join('/'));
+                      let end_date = new Date(dates?.[dates?.length - 1]?.date.split('/').reverse().join('/'));
+                      return(
+                        <>
+                          <div className={`discover-item`}>
+                            <div className="location-wrapper">
+                              {item.attributes.locations?.data?.map((loc,i) => {
+                                return(
+                                  <div>{loc.attributes.title}</div>
+                                )
+                              })}
+                            </div>
+                            <LazyLoad height={600}>
+                              <div className="item-wrapper">
+                                <a href={item.attributes.slug} key={'discover'+i}>
+                                  <div className="image">
+                                    {item.attributes.WhenWhere[0] && page.attributes.hide_when_where == true &&
+                                      <div className="info-overlay">
+                                        <div className="date">
+                                          { (Moment(start_date).format('MMM') == Moment(end_date).format('MMM') && dates.length > 1) ?
+                                            <>
+                                              {Moment(start_date).format('D')}{dates.length > 1 && <>–{Moment(end_date).format('D MMM')}</>}
+                                            </>
+                                          : 
+                                            <>
+                                              {Moment(start_date).format('D MMM')}   {dates.length > 1 && <>– {Moment(end_date).format('D MMM')}</>}
+                                            </>
+                                          }
+                                        </div>
+                                        <div className="times">
+                                          <div className="time">
+                                            <span>{item.attributes.WhenWhere[0].start_time}{item.attributes.WhenWhere[0].end_time && `—${item.attributes.WhenWhere[0].end_time}`}</span>
+                                          </div>
+                                        </div>
+                                      </div>
+                                    }
+                                    <div className="image-inner">
+                                      {item.attributes.cover_image?.data &&
+                                        <Image image={item.attributes.cover_image?.data?.attributes} layout='fill' objectFit='cover'/>
+                                      }
+                                      
+                                    </div>
+                                  </div>
+
+                                  <div className="category-title-wrapper">
+                                    <div className="title">{item.attributes.title}</div>
+                                  </div>
+                                </a>
+                              </div>
+                            </LazyLoad>
+                          </div>
+                        </>
+                      )
+                    })}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </>
+        }
+
         {relations.attributes.community_items.data.length > 0 &&
           <div className="discover artists">
             <div className="subtitle">
@@ -64,11 +161,11 @@ const SubProgrammeItem = ({page, global, relations, params, festival, sub, progr
             </div>
           </div>
         }
+
       </Layout>
     </section> 
   )
 }
-
 
 export async function getServerSideProps({params, query}) {
   const biennial = {
