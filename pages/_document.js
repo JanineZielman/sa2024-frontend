6c8849ec288a90b1a0b908d96c232cd3eae9e0b1
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2024-02-25 13:01:52 +0100

    second tag google

--- pages/_document.js
+++ pages/_document.js
@@ -52,6 +52,20 @@ class MyDocument extends Document {
               `,
             }}
           />
+           <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=GTM-N67ZLWP"
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'GTM-N67ZLWP', { 'anonymize_ip': true });
+              `,
+            }}
+          />
 
           <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-32x32.png" />
